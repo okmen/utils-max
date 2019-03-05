c987a490406120ae4a89f2bd2f4816fa6ae139cf
jiangjiayi <jiangjiayi@chudaokeji.com>
2019-03-05 14:18:37 +0800

    FileChannel 文件读写

--- src/main/java/com/utils_max/file/FileReadUtils.java
+++ src/main/java/com/utils_max/file/FileReadUtils.java
@@ -1,5 +1,6 @@
 package com.utils_max.file;
 
+import com.qiniu.util.StringUtils;
 import com.utils_max.file.bo.FileMetaRead;
 
 import java.io.File;
@@ -14,58 +15,63 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 public class FileReadUtils {
 
-	private static Map<String, FileMetaRead> mapReadFileMeta = new ConcurrentHashMap<String, FileMetaRead>();
-	private static ReentrantLock mapLock = new ReentrantLock();
+    private static Map<String, FileMetaRead> mapReadFileMeta = new ConcurrentHashMap<String, FileMetaRead>();
+    private static ReentrantLock mapLock = new ReentrantLock();
 
-	private static final int LENGTH_READ = 1024;
+    private static final int LENGTH_READ = 1024;
 
-	public static FileMetaRead readFile(String fileNameFull, long offset,
-			int readLength) throws IOException {
+    public static FileMetaRead readFile(String fileNameFull, long offset,
+                                        int readLength) throws IOException {
 
-		FileMetaRead readMeta = getFileMeta(fileNameFull);
-		if (readMeta.getCurrentFile() == null
-				|| !readMeta.getCurrentFile().exists()) {
-			readMeta.close();
-			return null;
-		}
-		readMeta.open();
-		if (offset > 0) {
-			readMeta.getChannel().position(offset);
-		}
+        FileMetaRead readMeta = getFileMeta(fileNameFull);
+        if (readMeta.getCurrentFile() == null
+                || !readMeta.getCurrentFile().exists()) {
+            readMeta.close();
+            return null;
+        }
+        readMeta.open();
+        if (offset > 0) {
+            readMeta.getChannel().position(offset);
+        }
 
-		if (readLength <= 0) {
-			readLength = LENGTH_READ;
-		}
-		long fileSize = readMeta.getChannel().size();
-		if ((readLength + offset) > fileSize) {
-			readLength = (int) (fileSize - offset);
-		}
+        if (readLength <= 0) {
+            readLength = LENGTH_READ;
+        }
+        long fileSize = readMeta.getChannel().size();
+        if ((readLength + offset) > fileSize) {
+            readLength = (int) (fileSize - offset);
+        }
 
-		ByteBuffer byteBuffer = ByteBuffer.allocate(readLength);
-		readMeta.getChannel().read(byteBuffer);
-		readMeta.setData(byteBuffer.array());
-		readMeta.setFileSize(fileSize);
-		readMeta.close();
-		return readMeta;
-	}
+        ByteBuffer byteBuffer = ByteBuffer.allocate(readLength);
+        readMeta.getChannel().read(byteBuffer);
+        readMeta.setData(byteBuffer.array());
+        readMeta.setFileSize(fileSize);
+        readMeta.close();
+        return readMeta;
+    }
 
-	private static FileMetaRead getFileMeta(String fileName) {
-		FileMetaRead meta = mapReadFileMeta.get(fileName);
-		if (meta != null) {
-			return meta;
-		}
-		try {
-			mapLock.lock();
-			File file = new File(fileName);
-			meta = new FileMetaRead(file);
-			mapReadFileMeta.put(fileName, meta);
-			return meta;
-		} catch (Exception e) {
-			// TODO
-		} finally {
-			mapLock.unlock();
-		}
-		return meta;
-	}
+    private static FileMetaRead getFileMeta(String fileName) {
+        FileMetaRead meta = mapReadFileMeta.get(fileName);
+        if (meta != null) {
+            return meta;
+        }
+        try {
+            mapLock.lock();
+            File file = new File(fileName);
+            meta = new FileMetaRead(file);
+            mapReadFileMeta.put(fileName, meta);
+            return meta;
+        } catch (Exception e) {
+            // TODO
+        } finally {
+            mapLock.unlock();
+        }
+        return meta;
+    }
 
+    public static void main(String[] arg) {
+        String aa = "sss";
+        System.out.print(StringUtils.isNullOrEmpty(aa));
+
+    }
 }
